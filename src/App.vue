aadee26e811baafb06e6b40e0e73e1c96118311c
Stephen Englert <stevee2112@gmail.com>
2020-03-28 17:52:44 -0400

    getting close

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,87 @@
     <KdHeader/>
 	<KdVoter v-for="player in players"
 			v-bind:key="player.position"
+			v-bind:playerIndex="player.position"
 			v-bind:leader="player.leader"
 			v-bind:name="player.name"
+			v-bind:vote="player.vote"
 			v-bind:power="player.power"
 			v-bind:moderator="player.moderator"
+			v-bind:active="player.active"
+			v-on:voted="handleVote"
 	/>
+	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addPlayerModal">
+		Add Player
+	</button>
 	<button type="button" class="btn btn-primary" v-on:click="changeLeader">Change Leader</button>
+	<button type="button" class="btn btn-primary" v-on:click="changeModerator">Change Moderator</button>
+	<button type="button" class="btn btn-danger" v-on:click="newVote">New Vote</button>
+
+	<div class="modal fade" id="addPlayerModal" ref="addPlayerModal" tabindex="-1" role="dialog" aria-labelledby="addPlayerModalLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<h5 class="modal-title" id="addPlayerModalLabel">Add Player</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			  <span aria-hidden="true">&times;</span>
+			</button>
+		  </div>
+		  <div class="modal-body">
+			<form>
+			  <div class="form-group row">
+				<label for="inputName" class="col-sm-2 col-form-label">Name</label>
+				<div class="col-sm-10">
+				  <input type="text" class="form-control" id="inputName" placeholder="Name">
+				</div>
+			  </div>
+			  <div class="form-group row">
+				<label for="inputPower" class="col-sm-2 col-form-label">Power</label>
+				<div class="col-sm-10">
+				  <input type="text" class="form-control" id="inputPower" placeholder="Power">
+				</div>
+			  </div>
+			</form>
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+			<button type="button" class="btn btn-primary" v-on:click="addPlayer">Add</button>
+		  </div>
+		</div>
+	  </div>
+	</div>
   </div>
 </template>
 
 <script>
 import KdVoter from './components/KdVoter.vue'
 import KdHeader from './components/KdHeader.vue'
+import $ from 'jquery'
 
 export default {
   name: 'App',
   data : function () {
 	return {
-		globalPower: 3,
+		currentLeader: 0,
+		currentModerator: 1,
+		currentPlayer: 0,
 		players: [
 			{
 				position: 0,
 				name: "Steve",
-				power: 2,
+				power: 0,
 				leader: true,
-				moderator: false
+				moderator: false,
+				active: true,
+				vote: "",
 			},
 			{
 				position: 1,
 				name: "Megan",
-				power: 6,
-				isLeader: false,
-				moderator: true
+				power: 0,
+				leader: false,
+				moderator: true,
+				active: false,
+				vote: "",
 			}
 		]
 	}
@@ -44,9 +93,43 @@ export default {
     KdVoter
   },
   methods: {
+	getNext: function() {
+		return (this.currentPlayer + 1) % this.players.length;
+	},
+	handleVote: function(voteData) {
+		this.$set(this.players[voteData.player], "power", this.players[voteData.player].power + voteData.power)
+		this.$set(this.players[voteData.player], "vote", voteData.vote)
+		this.$set(this.players[voteData.player], "active", false)
+
+		var nextPlayer = this.getNext();
+		this.currentPlayer = nextPlayer;
+		this.$set(this.players[nextPlayer], "active", true)
+	},
+	newVote: function() {
+		for (var i = 0; i < this.players.length;i++) {
+			this.$set(this.players[i], "power", 0);
+			this.$set(this.players[i], "vote", "");
+		}
+	},
     changeLeader: function() {
-		this.$set(this.players[0], "leader", false)
-		var nextPlayerPosition = this.players.length;
+		this.$set(this.players[this.currentLeader], "leader", false)
+		
+		this.currentLeader++;
+		if (this.currentLeader >= this.players.length) {
+			this.currentLeader = 0;
+		}
+
+		this.$set(this.players[this.currentLeader], "leader", true)
+	},
+    changeModerator: function() {
+		this.$set(this.players[this.currentModerator], "moderator", false)
+		
+		this.currentModerator++;
+		if (this.currentModerator >= this.players.length) {
+			this.currentModerator = 0;
+		}
+
+		this.$set(this.players[this.currentModerator], "moderator", true)
 	},
     addPlayer: function() { // TODO
 		this.$set(this.players[0], "leader", false)
@@ -58,6 +141,7 @@ export default {
 			isLeader: false,
 			moderator: true
 		});
+		$('#addPlayerModal').modal('hide')
 	},
   }
 }
@@ -71,5 +155,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.btn {
+	margin-left: 10px;
+	margin-right: 10px;
 }
 </style>
--- src/components/KdVoter.vue
+++ src/components/KdVoter.vue
@@ -1,36 +1,146 @@
 <template>
-	<div class="alert alert-secondary" role="alert">
+	<div class="alert" v-bind:class="{
+		'alert-secondary': vote == '',
+		'alert-danger' : vote == 'nay',
+		'alert-primary' : vote == 'aye',
+		'alert-light' : vote == 'passPower',
+		'alert-light' : vote == 'passModerator',
+	}" role="alert">
 		<div class="container">
 		  <div class="row">
 			<div class="col">
 			  <h1>{{ leader ? "X" : "" }}</h1>
 			</div>
 			<div class="col">
-			  <h1>{{ name }}</h1>
+			  <h1>{{ displayName }}</h1>
 			</div>
 			<div class="col">
-			  <h1>{{ vote }}</h1>
+			  <h1><span :hidden="active == true">{{ displayVote }}</span></h1>
+			  <button type="button" class="btn btn-primary" data-toggle="modal" :data-target="this.makeIdRef('voteModal')" :hidden="(vote != '') || (active == false)">Vote</button>
+			  <button type="button" class="btn btn-primary" data-toggle="modal" :data-target="this.makeIdRef('voteModal')" :hidden="(vote == '') || (active == false)">Add Power</button>
 			</div>
 			<div class="col">
-			  <h1>{{ power }}</h1>
+			  <h1>{{ power > 0 ? power : ""  }}</h1>
 			</div>
 			<div class="col">
 			  <h1>{{ moderator ? "X" : ""}}</h1>
 			</div>
 		  </div>
 		</div>
+	<div class="modal fade" :id="this.makeId('voteModal')" ref="voteModal" tabindex="-1" role="dialog" aria-labelledby="voteModalLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<h5 class="modal-title" id="voteModalLabel">Vote</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			  <span aria-hidden="true">&times;</span>
+			</button>
+		  </div>
+		  <div class="modal-body">
+			<form>
+			  <fieldset class="form-group" :hidden="vote != ''">
+				<div class="row">
+				  <div class="col-sm-10">
+					<div class="form-check">
+					  <input class="form-check-input" type="radio" name="gridRadios" :id="this.makeId('gridRadios1')" v-model="inputVote" value="aye">
+					  <label class="form-check-label" :for="this.makeId('gridRadios1')">
+						Aye
+					  </label>
+					</div>
+					<div class="form-check">
+					  <input class="form-check-input" type="radio" name="gridRadios" :id="this.makeId('gridRadios2')" v-model="inputVote" value="nay">
+					  <label class="form-check-label" :for="this.makeId('gridRadios2')">
+						Nye
+					  </label>
+					</div>
+					<div class="form-check">
+					  <input class="form-check-input" type="radio" name="gridRadios" :id="this.makeId('gridRadios3')" v-model="inputVote" value="passPower">
+					  <label class="form-check-label" :for="this.makeId('gridRadios3')">
+						Pass and Gather Power
+					  </label>
+					</div>
+					<div class="form-check">
+					  <input class="form-check-input" type="radio" name="gridRadios" :id="this.makeId('gridRadios4')" v-model="inputVote" value="passModerator">
+					  <label class="form-check-label" :for="this.makeId('gridRadios4')">
+						Pass and Become Moderator
+					  </label>
+					</div>
+				  </div>
+				</div>
+			  </fieldset>			
+			  <div class="form-group row" :hidden="inputVote == 'passPower' || inputVote == 'passModerator'">
+				<label for="inputName" class="col-sm-2 col-form-label">Power</label>
+				<div class="col-sm-10" hid>
+				  <input type="text" class="form-control" :id="this.makeId('powerInput')" v-model="inputPower" placeholder="Power">
+				</div>
+			  </div>
+			</form>
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+			<button type="button" class="btn btn-primary" v-on:click="submitVote">Vote</button>
+		  </div>
+		</div>
+	  </div>
+	</div>
+
 	</div>
 </template>
 
 <script>
+import { v4 as uuid } from 'uuid';
+import $ from 'jquery'
+
 export default {
   name: 'kdVoteTracker',
   props: {
+	  playerIndex: Number,
 	  leader: Boolean,
       name: String,
 	  vote: String,
 	  power: Number,
-	  moderator: Boolean
+	  moderator: Boolean,
+	  active: Boolean,
+  },
+  data: function() {
+	return {
+		uuid:  uuid(),
+		inputVote: this.vote,
+		inputPower: "",
+		
+	}
+  },
+  computed: {
+	displayName: function() {
+		return this.name + (this.active ? " is up" : "");
+	},
+	displayVote: function() {
+		var voteDisplayMap = {
+			"": "",
+			"aye": "Aye",
+			"nay": "Nay",
+			"passPower": "Pass and Gather Power",
+			"passModerator": "Pass and Become Moderator",
+		}
+
+		return voteDisplayMap[this.vote];
+	}
+  },
+  methods: {
+	makeIdRef: function(id) {
+		return "#" + this.makeId(id);
+	},
+	makeId: function(id) {
+		return id + this.uuid;
+	},
+	submitVote: function() {
+		this.$emit('voted', {
+			player: this.playerIndex,
+			vote: this.inputVote,
+			power: parseInt(this.inputPower)
+		});
+		$(this.makeIdRef("voteModal")).modal('hide');
+	}
   }
 }
 </script>
